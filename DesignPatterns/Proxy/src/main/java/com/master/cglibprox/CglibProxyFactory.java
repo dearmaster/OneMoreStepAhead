ca3f310a05c92e80c779a5c1b7e140fbc935aefa
dearmaster windows7 <694114533@qq.com>
2018-04-10 00:59:03 +0800

    design pattern: proxy

--- DesignPatterns/Proxy/src/main/java/com/master/cglibprox/CglibProxyFactory.java
+++ DesignPatterns/Proxy/src/main/java/com/master/cglibprox/CglibProxyFactory.java
@@ -24,6 +24,7 @@ public class CglibProxyFactory {
                 method.invoke(obj, args);
                 System.out.println("Ending...");
                 System.out.println(proxy.getClass());
+//                System.out.println(proxy); //Exception throws here
                 return proxy;
             }
         });
